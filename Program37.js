afb31c6fb3207ada3f807446fe09f2e4a44d7262
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-07-19 00:06:57 +0530

    Create Program37.js to demonstrate array operations
    Implement a simple JavaScript program that demonstrates array usage and modification.

---
+++ Program37.js
@@ -0,0 +1,17 @@
+//Javascript Array
+
+function main()
+{
+  let Arr = [10,20,30,40];//use const as well let
+
+  console.log(Arr[0]);
+  console.log(Arr[1]);
+  console.log(Arr[2]);
+  console.log(Arr[3]);
+
+  Arr[0] = 11;
+
+  console.log(Arr[0]);
+}
+
+main();
