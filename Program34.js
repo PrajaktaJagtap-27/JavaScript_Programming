a2d8695369881b86ec333d70e815a2b7ec0d67fc
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-07-19 00:04:27 +0530

    Add fat arrow function for addition

---
+++ Program34.js
@@ -0,0 +1,16 @@
+//Actual fat arrow function
+Addition = (A,B) =>  A+B;
+
+/*
+function Addition(A,B)
+{
+  return A+B;
+}*/
+function main()
+{
+  let Ret = 0;
+  Ret = Addition(10,11);  //10+11
+  console.log("Addition is :",Ret);
+}
+
+main();
