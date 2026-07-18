094eb5b82b37a41cae50abc36cc8fb41bca438ea
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-07-19 00:06:19 +0530

    Add Program36.js to demonstrate JavaScript arrays

---
+++ Program36.js
@@ -0,0 +1,12 @@
+//Javascript Array
+
+function main()
+{
+  let Arr = [10,20,30,40];//use const as well let
+
+  console.log(Arr);
+  console.log(typeof(Arr));
+  console.log(Arr.length);
+}
+
+main();
