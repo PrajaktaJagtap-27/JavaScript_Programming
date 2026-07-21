0dd462c13a1c983b4ab39c286b02bb73a5f0f56d
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-07-21 20:31:24 +0530

    Add main function to access JSON object properties

---
+++ Program40.js
@@ -0,0 +1,15 @@
+//Accessing jSON
+
+function main()
+{
+  let obj = {name : "Prajakta",salary : 21000};
+  //console.log(obj[0]);  Not Allowed
+
+  console.log(obj.name);
+  console.log(obj.salary);
+
+ // console.log(obj[name]);  //Not Allowed
+}
+
+  
+main();
